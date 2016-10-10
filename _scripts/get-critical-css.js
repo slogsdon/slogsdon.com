956f1ec7bf1b5557dc61de7df324587ce1039c22
Shane Logsdon <shane@shanelogsdon.com>
2016-10-09 21:54:26 -0400

    optimize css delivery

---
+++ _scripts/get-critical-css.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const critical = require('critical');
+
+critical.generate({
+    base: '_site/',
+    src: 'index.html',
+    css: ['assets/css/main.css'],
+    dest: 'assets/css/main.critical.css',
+    dimensions: [{
+        height: 200,
+        width: 500
+    }, {
+        height: 900,
+        width: 1200
+    }]
+});
