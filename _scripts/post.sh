fe7094c972715853ec299eb25c668ab635d1599d
Shane Logsdon <shane@shanelogsdon.com>
2017-01-22 21:05:38 -0500

    add html minification

--- _scripts/post.sh
+++ _scripts/post.sh
@@ -2,3 +2,4 @@
 
 node _scripts/generate-css.js
 node _scripts/generate-critical-css.js
+node_modules/.bin/html-minifier -c html-minifier.json --input-dir public --output-dir public --file-ext html && echo "minified HTML"
