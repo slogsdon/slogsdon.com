56ea69d9c1f25aaa638f6fe1b6537471c3412c16
Shane Logsdon <shane@shanelogsdon.com>
2018-01-30 23:45:14 -0500

    set viewport meta

--- components/blank-layout.js
+++ components/blank-layout.js
@@ -3,7 +3,7 @@ import Head from "next/head";
 const BlankLayout = ({ children }) => (
   <div>
     <Head>
-      <html lang="en" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
     </Head>
 
     {children}
